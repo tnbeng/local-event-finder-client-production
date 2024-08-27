a83da1a5417eff32c411ed86e7ce141a82881ece
Tarak Nath Bar <btarak398@gmail.com>
2024-08-28 03:50:09 +0530

    first upload

---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Event from '../components/Event';
+
+const Home = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+     try{
+      const res = await axios.get('https://local-event-finder-tarak.onrender.com/events');//updated url
+      console.log("Response ",res)
+      alert("Response "+res.data)
+      setEvents(res.data);
+     }
+     catch(err)
+     {
+      console.log("Error ",err)
+      alert("Something went wrong",error )
+     }
+    };
+    fetchEvents();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Upcoming Events</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {events.map((event) => (
+          <Event key={event._id} event={event} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
